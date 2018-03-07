1cf755dca30bfc4180535840c44ea2ac59672eb6
Gabriel Pérez S <gabriel@guero.net>
2018-03-07 21:16:01 +0100

    Code comments

--- redis/fs/directory.go
+++ redis/fs/directory.go
@@ -10,51 +10,76 @@ import (
 )
 
 var (
-	defaultTTL int64 = 60 * 60 // Seconds
+	// defaultDirTTL is the time after that the directory will be removed from the cache
+	defaultDirTTL = time.Minute * -1
+	// defaultCleanInterval is the interval time to delete old records from the cache
+	defaultCleanInterval = time.Second * 1
 )
 
-type dirTTL struct {
+// MkDirCache store in memory the existing directories created
+type MkDirCache struct {
 	sync.Mutex
 	m        sync.Map
 	creating map[string]sync.Once
-	tick     *time.Ticker
 }
 
-func NewDirCache() *dirTTL {
-	d := &dirTTL{
+// NewDirCache is a process create a directory just for once
+func NewDirCache() *MkDirCache {
+	d := &MkDirCache{
 		creating: make(map[string]sync.Once),
-		tick:     time.NewTicker(1 * time.Second),
 	}
+	// Run a goroutine to remove the directory from the local cache
 	go d.clean()
 	return d
 }
 
-func (d *dirTTL) clean() {
+func (d *MkDirCache) clean() {
 	for {
-		<-d.tick.C
+		// Wait for the next check
+		time.Sleep(defaultCleanInterval)
+
+		// deadLine is the limit timestamp to keep a directory in the cache
+		deadLine := time.Now().Add(defaultDirTTL).Unix()
+
+		// Read all the values in the map and check if are older than the deadLine of the prev line
 		d.m.Range(func(key, val interface{}) bool {
-			if time.Now().Unix() > val.(int64)+defaultTTL {
+			if val.(int64) < deadLine {
+				// If the record is older than the deadLine delete it
 				s := key.(string)
 				d.m.Delete(s)
-				log.Printf("FS dirTTL delete: %s", s)
+				lib.Debugf("FS MkDirCache delete: %s", s)
 			}
 			return true
 		})
 	}
 }
 
-func (d *dirTTL) makeAll(key string) error {
+// makeAll is a function to create a directory just one time
+// and store it in a sync.Map as local cache. The process to
+// create a directory require some I/O to the filesystem that
+// we meed to reduce. Also require to parse (Unmarshal) the path
+// and check level by level in the directory tree. Storing in
+// the cache and checking from there is a way to reduce all this
+// steps at each call.
+func (d *MkDirCache) makeAll(key string) error {
+	// Check if the directory is in the cache
 	if _, ok := d.m.Load(key); ok {
-		d.m.Store(key, time.Now().Unix())
 		return nil
 	}
 
-	// Add or recover from the creation map
+	// The process for create a directory could be called from
+	// many goroutines so we need to know if another goroutine is
+	// creating the directory and be sure that only one of them
+	// create the directory. The other threads should wait until
+	// the first thread create the directory. The following map
+	// store the sync.Once struct using the directory name as key.
 	d.Lock()
 	_, ok := d.creating[key]
 	if !ok {
+		// If the directory is not in the map we add it
 		d.creating[key] = sync.Once{}
 	}
+	// We recover the directory from the map
 	mkDirOnce := d.creating[key]
 	d.Unlock()
 
@@ -67,11 +92,12 @@ func (d *dirTTL) makeAll(key string) error {
 		}
 		d.m.Store(key, time.Now().Unix())
 
-		// Delete from the creation map
+		// If the directory was created sucessfully we procede
+		// to remove the directory from the map
 		d.Lock()
-		lib.Debugf("FS dirTTL create: %s", key)
 		delete(d.creating, key)
 		d.Unlock()
+		lib.Debugf("FS MkDirCache create: %s", key)
 	})
 
 	return nil
