b994779bd9f247d051f147d52107f98cdec581f5
Sebastià Serra Rigo <sebastia.s@dotw.com>
2020-01-08 17:33:36 +0100

    Updated vendor

---
+++ vendor/github.com/savsgio/gotils/conv.go
@@ -0,0 +1,35 @@
+package gotils
+
+import (
+	"reflect"
+	"unsafe"
+)
+
+// B2S converts byte slice to a string without memory allocation.
+// See https://groups.google.com/forum/#!msg/Golang-Nuts/ENgbUzYvCuU/90yGx7GUAgAJ .
+//
+// Note it may break if string and/or slice header will change
+// in the future go versions.
+func B2S(b []byte) string {
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&b))
+	bh := reflect.SliceHeader{
+		Data: sh.Data,
+		Len:  sh.Len,
+		Cap:  sh.Len,
+	}
+	return *(*string)(unsafe.Pointer(&bh))
+}
+
+// S2B converts string to a byte slice without memory allocation.
+//
+// Note it may break if string and/or slice header will change
+// in the future go versions.
+func S2B(s string) []byte {
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	bh := reflect.SliceHeader{
+		Data: sh.Data,
+		Len:  sh.Len,
+		Cap:  sh.Len,
+	}
+	return *(*[]byte)(unsafe.Pointer(&bh))
+}
